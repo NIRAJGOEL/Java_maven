00884b300c49822e14d48c3adf176e76e90711cf
Niraj Goel <nirajgoel123@gmail.com>
2018-04-30 07:25:46 +0000

    More program

---
+++ mybmw-app/src/main/java/com/bmw/app/App.java
@@ -0,0 +1,14 @@
+package com.bmw.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "This is Niraj first Java program ha ha!!" );
+    }
+}
